33578422e5050a5de01a477933f58fecd8b4dd46
ozer-a1 <ozer.unat@agilone.com>
2014-04-06 22:24:12 -0500

    DL002 - Server port

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var express = require('express')
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT );
+  app.set('port', process.env.PORT || 1337);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
